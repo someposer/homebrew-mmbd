7f19518e8223394bf0c2697ca8c7e7666d614fce
Paul Bailey <paul.bailey@rinbo.consulting>
2016-08-08 14:04:36 +0100

    Oops, switch from sha1 to sha256

--- libmmbd.rb
+++ libmmbd.rb
@@ -14,7 +14,7 @@ class Libmmbd < Formula
 	url "http://www.makemkv.com/download/makemkv_v1.10.0_osx.dmg", :using => DmgDownloadStrategy
 	homepage "http://www.makemkv.com/download/"
 	version "1.10.0"
-	sha1 "fc0f403c24b687d741780578be50832e14b199ba5ae9119020ec997804e65dd2"
+	sha256 "fc0f403c24b687d741780578be50832e14b199ba5ae9119020ec997804e65dd2"
 
 	conflicts_with "libaacs", :because => "This formula implements libaacs as well as libbdplus"
 
