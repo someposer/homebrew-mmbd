304ed676a33f45e3dd701af860531112246c234d
Daniel Rothmaler <drothmaler@googlemail.com>
2016-10-11 10:28:08 +0200

    Updated to version 1.10.2

--- libmmbd.rb
+++ libmmbd.rb
@@ -11,10 +11,10 @@ end
 
 
 class Libmmbd < Formula
-	url "http://www.makemkv.com/download/makemkv_v1.10.1_osx.dmg", :using => DmgDownloadStrategy
+	url "http://www.makemkv.com/download/makemkv_v1.10.2_osx.dmg", :using => DmgDownloadStrategy
 	homepage "http://www.makemkv.com/download/"
-	version "1.10.1"
-	sha256 "39e6bfb950dc2edce333f145b2e69aca26f8215004868790d0343fe45a9e4de6"
+	version "1.10.2"
+	sha256 "cb2dd115242e2ab0c11aa1e167aa44337094410a621855326c90f6b5dd6f40c7"
 
 	conflicts_with "libaacs", :because => "This formula implements libaacs as well as libbdplus"
 
